d3cd1e45f664e53b56d7d05d9e4a4d17aab23225
Marina Belova <salirsky@mail.ru>
2022-04-14 13:47:39 +0300

    Color changes but wrong

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+"use strict";
 
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
+const square = document.getElementById("square");
 
+const colorChange = function (event) {
+  square.style.backgroundColor = event.target.value;
+};
 
+//text.addEventListener("input", colorChange);
 
-
+btn.addEventListener("click", function () {
+  console.log("Клик по кнопке");
+  text.addEventListener("input", colorChange);
+});
+// В таком виде цвет меняется, только если кнопка уже была кликнута, причём цвет меняется как только его ввели, независимо от нажатия кнопки. Если кнопка кликнута не была, цвет не изменится, пока на неё не нажать.
