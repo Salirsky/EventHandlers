367b3bebd107d3a0125d24cf2a15e75bbe0de4c3
Marina Belova <salirsky@mail.ru>
2022-04-14 13:52:43 +0300

    Added dependence of circle size on range

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const btn = document.getElementById("btn");
 const text = document.getElementById("text");
 const square = document.getElementById("square");
 const eBtn = document.getElementById("e_btn");
+const range = document.getElementById("range");
+const rangeSpan = document.getElementById("range-span");
+const circle = document.getElementById("circle");
 
 const colorChange = function (event) {
   square.style.backgroundColor = event.target.value;
@@ -18,3 +21,11 @@ btn.addEventListener("click", function () {
 // В таком виде цвет меняется, только если кнопка уже была кликнута, причём цвет меняется как только его ввели, независимо от нажатия кнопки. Если кнопка кликнута не была, цвет не изменится, пока на неё не нажать.
 
 eBtn.style.display = "none";
+
+const rangeLogger = function (event) {
+  rangeSpan.textContent = event.target.value + "%";
+  circle.style.width = event.target.value + "%";
+  circle.style.height = event.target.value + "%";
+};
+
+range.addEventListener("input", rangeLogger);
